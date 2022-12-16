be27b28b7634470b5ff0740492a5141745a874d6
Hyunggiiii <119280639+Hyunggiiii@users.noreply.github.com>
2022-12-16 17:33:52 +0900

    12/16
    Enemy_2

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -9,16 +9,34 @@ public class PlayerManager : MonoBehaviour
     PlayerMove pm;
 
     public Text lifeText;
+
+    Rigidbody rb;
+    public int isCollisionPower;
+
+    public int enemy_2Power;
+
     private void Start()
     {
         pm = GetComponent<PlayerMove>();
-
+        rb = GetComponent<Rigidbody>();
         lifeText.text = $"Life = {playerHP}";
     }
     private void Update()
     {
         CheckHP();
         lifeText.text = $"Life = {playerHP}";
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Enemy_2")
+        {
+            rb.velocity = Vector3.up * isCollisionPower;
+            playerHP -= enemy_2Power;
+            //Vector3 dir = transform.position - collision.gameObject.transform.position;
+            //dir = dir.normalized * 1000;
+            //collision.gameObject.GetComponent<Rigidbody>().AddForce(dir);
+        }
     }
 
     void CheckHP()
