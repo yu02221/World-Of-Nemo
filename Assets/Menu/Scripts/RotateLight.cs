e5bd62823ee6dc168ba37348a33ff0786657b23e
skaqls <119280684+skaqls@users.noreply.github.com>
2022-12-23 17:33:52 +0900

    밤낮 구성 완(꾸미기 중)

--- Assets/Menu/Scripts/RotateLight.cs
+++ Assets/Menu/Scripts/RotateLight.cs
@@ -6,8 +6,29 @@ public class RotateLight : MonoBehaviour
 {
     public float liightSpeed;
 
+    public GameObject Target;
+
+    public float offsetX = 0.0f;
+    public float offsetY = 0.0f; 
+    public float offsetZ = 0.0f;
+
+    public float SKYSpeed = 10.0f;
+    Vector3 TargetPos;
+
     private void Update()
     {
         transform.Rotate(Vector3.right, (liightSpeed) * Time.deltaTime);
     }
+
+    void FixedUpdate()
+    {
+        TargetPos = new Vector3(
+            Target.transform.position.x + offsetX,
+            Target.transform.position.y + offsetY,
+            Target.transform.position.z + offsetZ
+            );
+
+        transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * SKYSpeed);
+    }
 }
+
